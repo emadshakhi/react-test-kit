e39579f5ba25b84b9c1f0ef1639fc437ff084b5c
e.shakhi <e.shakhi@keepa.ir>
2024-07-01 08:49:12 +0330

    added app shit

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,43 @@ import React, { useEffect, useRef } from "react";
 
 export {};
 function App() {
-  return <div>sdfsfd</div>;
+  const [state, setState] = React.useState(false);
+
+  const buttonRef = useRef(null);
+
+  const theme = createCustomTheme({});
+
+  // const rtlCache = createCache({
+  //   key: "muirtl",
+  //   stylisPlugins: [prefixer, rtlPlugin],
+  // });
+
+  useEffect(() => {
+    console.log(buttonRef);
+  }, [buttonRef]);
+
+  return (
+    // <CacheProvider value={rtlCache}>
+    <ThemeProvider theme={theme}>
+      <Button
+        ref={buttonRef}
+        variant="contained"
+        onClick={() => setState(!state)}
+        sx={{
+          bgcolor: (theme) => {
+            //
+            console.log("dd", theme);
+
+            return "red";
+          },
+        }}
+      >
+        sdfdf
+      </Button>
+      <Typography variant="">sdfds</Typography>
+    </ThemeProvider>
+    // </CacheProvider>
+  );
 }
 
 export default App;
