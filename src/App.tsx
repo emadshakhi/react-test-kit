350e9e2afad957fe31a1c430634bee10c89f2b0a
e.shakhi <e.shakhi@keepa.ir>
2024-07-01 08:44:39 +0330

    empty app

--- src/App.tsx
+++ src/App.tsx
@@ -6,43 +6,7 @@ import React, { useEffect, useRef } from "react";
 
 export {};
 function App() {
-  const [state, setState] = React.useState(false);
-
-  const buttonRef = useRef(null);
-
-  const theme = createCustomTheme({});
-
-  // const rtlCache = createCache({
-  //   key: "muirtl",
-  //   stylisPlugins: [prefixer, rtlPlugin],
-  // });
-
-  useEffect(() => {
-    console.log(buttonRef);
-  }, [buttonRef]);
-
-  return (
-    // <CacheProvider value={rtlCache}>
-    <ThemeProvider theme={theme}>
-      <Button
-        ref={buttonRef}
-        variant="contained"
-        onClick={() => setState(!state)}
-        sx={{
-          bgcolor: (theme) => {
-            //
-            console.log("dd", theme);
-
-            return "red";
-          },
-        }}
-      >
-        sdfdf
-      </Button>
-      <Typography variant="">sdfds</Typography>
-    </ThemeProvider>
-    // </CacheProvider>
-  );
+  return <div>sdfsfd</div>;
 }
 
 export default App;
